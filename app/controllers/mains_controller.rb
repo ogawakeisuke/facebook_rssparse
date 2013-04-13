edd12e922b49ed0b7f3fa7ad35afd1fbfcaee5d3
ogawakeisuke <ogawa@uniba.jp>
2013-04-13 18:54:57 +0900

    いったんデプ

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class MainsController < ApplicationController
   require 'rss'
   require 'kconv'
@@ -6,9 +8,14 @@ class MainsController < ApplicationController
     uri = "http://www.facebook.com/feeds/page.php?format=rss20&id=238534679512353"
     rss = RSS::Parser.parse(uri, true)
     @descs = rss.channel.items.collect {|item| item.description.split(/(<br\/>)|(<br\s\/>)/).reject {|arr| arr =~ /<br\/>|<br\s\/>/}.delete_if{|item|item.blank?} }
-    # descs.each do |description|
-    #   @descs = description.delete_if{|item|item.blank?}
-    #   logger.debug(description.delete_if{|item|item.blank?})
-    # end
+
+    #
+    # ここからスクランブル
+    #
+    @title_collections = @descs.collect { |desc| desc[0] }.flatten
+    @desc_collections = @descs.collect { |desc| desc.drop(1).delete_if {|description| description =~ /<.*.>|＿.*.＿/} }.flatten
+    @img_tag_collections = @descs.collect {|desc| desc.select {|description| description =~ /<.*.>/ } }.flatten
+    # @img_url = img_tag_collections.sample =~ /src\=\"/
+    # logs @img_url
   end
 end
