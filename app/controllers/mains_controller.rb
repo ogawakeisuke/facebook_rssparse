202accd84345ed9c7f4dd3f104f6c6bf83989542
ogawakeisuke <ogawa@uniba.jp>
2013-04-13 15:30:51 +0900

    aaa

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -5,6 +5,10 @@ class MainsController < ApplicationController
   def index
     uri = "http://www.facebook.com/feeds/page.php?format=rss20&id=238534679512353"
     rss = RSS::Parser.parse(uri, true)
-    @descs = rss.channel.items.collect {|item| item.description.split("<br /> <br /> <br />") }
+    @descs = rss.channel.items.collect {|item| item.description.split(/(<br\/>)|(<br\s\/>)/).reject {|arr| arr =~ /<br\/>|<br\s\/>/}.delete_if{|item|item.blank?} }
+    # descs.each do |description|
+    #   @descs = description.delete_if{|item|item.blank?}
+    #   logger.debug(description.delete_if{|item|item.blank?})
+    # end
   end
 end
