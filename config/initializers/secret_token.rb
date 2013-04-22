f96304becde77826a4810261c999b490ec335438
ogawakeisuke <ogawa@uniba.jp>
2013-04-22 14:21:32 +0900

    token変えてみたけど

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FacebookRssparse::Application.config.secret_token = 'a92c0ecd82211ebab72fe32867d6a298eb9a59a6813e9724420c1a25fc7aaf82588b4b4d176ff41d21a0a8b3bd756446e34da1c180550c34a5799f60cfec1ff3'
+FacebookRssparse::Application.config.secret_token = 'BAAGg2CjHYLkBAP8tqvZBUwnPDSXBSHVUB1f7Gk7LLLYqqG9nUwQ0YRuqRiNY9Ao5FaSjBW1S1CQ8aZAIxeHMSBpxorqopVh27muPidZBIAkupZB6jH0OItCki7LpMGcdUPbUhxiTnOAVKApeOi8ufIDDcogSkprrZBSNc97cbVFzgtCuhDcv21WZBuNCDDMN6aFvZCBlLdqZBBUZAqxKjLWW3'
