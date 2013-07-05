901b84ab0ba6a7849e09d0c2fe8bcf0fc19d9586
ogawakeisuke <ogawa@uniba.jp>
2013-07-05 11:15:57 +0900

    publish_actionsに変更

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@
 # app_id = '458325234245817'
 # app_secret = '64757242b2bdd251e1303ea5e0346324'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook , FB_APP_ID , FB_APP_SECRET  , { :scope => 'publish_stream, offline_access,  photo_upload' }
+  provider :facebook , FB_APP_ID , FB_APP_SECRET  , { :scope => 'publish_actions, offline_access,  photo_upload' }
 end
